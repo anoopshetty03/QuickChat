d8e39f121cdba9a5a808a88ef0471f760b603452
anoopshetty03 <shettyanoop2003@gmail.com>
2024-09-09 20:48:45 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  mongoose
+  .connect(process.env.DATABASE_HOST + process.env.DATABASE_NAME, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`${process.env.DATABASE_NAME} db connected successfully`);
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log('db error');
+    process.exit(1);
+  });
+};
+
+module.exports = connectDB;
+
